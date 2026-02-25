9cb326934bf9d751f8657907d4ad7fa0239c56f7
Yamuna H <70136290+yamunah96@users.noreply.github.com>
2026-02-25 15:52:29 +0530

    Add files via upload

---
+++ Machine_learning_Fastapi/frontend.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8000/predict" 
+
+st.title("Insurance Premium Category Predictor")
+st.markdown("Enter your details below:")
+
+# Input fields
+age = st.number_input("Age", min_value=1, max_value=119, value=30)
+weight = st.number_input("Weight (kg)", min_value=1.0, value=65.0)
+height = st.number_input("Height (m)", min_value=0.5, max_value=2.5, value=1.7)
+income_lpa = st.number_input("Annual Income (LPA)", min_value=0.1, value=10.0)
+smoker = st.selectbox("Are you a smoker?", options=[True, False])
+city = st.text_input("City", value="Mumbai")
+occupation = st.selectbox(
+    "Occupation",
+    ['retired', 'freelancer', 'student', 'government_job', 'business_owner', 'unemployed', 'private_job']
+)
+
+if st.button("Predict Premium Category"):
+    input_data = {
+        "age": age,
+        "weight": weight,
+        "height": height,
+        "income_lpa": income_lpa,
+        "smoker": smoker,
+        "city": city,
+        "occupation": occupation
+    }
+
+    try:
+        response = requests.post(API_URL, json=input_data)
+        print(response)
+        result = response.json()
+        print(result)
+        # Directly access predicted_category
+        prediction = result["predicted_category"]
+        if response.status_code == 200 and result:
+            print(prediction)
+            st.success(f"Predicted Insurance Premium Category: **{prediction}**")
+    
+
+        else:
+            st.error(f"API Error: {response.status_code}")
+            st.write(result)
+
+    except requests.exceptions.ConnectionError:
+        st.error("❌ Could not connect to the FastAPI server. Make sure it's running.")
+
+    except requests.exceptions.HTTPError as err:
+        st.error(f"HTTP error occurred: {err}")
+        st.write(response.text)
+
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
